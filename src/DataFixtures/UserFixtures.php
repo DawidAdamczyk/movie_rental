e9e5e24a3a63a78e7ea71f80daa8d0904f4566b9
Dawid <dawidadamczykeit@gmail.com>
2019-10-26 15:33:48 +0200

    basic login

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+         $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('dupa@gmail.com');
+        $user->setName('Kacper');
+        $user->setLastname('Orzech');
+        $user->setCardNumber(12345);
+        $user->setRoles([]);
+        $user->setBlocked(false);
+        $user->setPassword($this->passwordEncoder->encodePassword(
+                        $user,
+                         'dupa'
+    ));
+    $manager->persist($user);
+    $manager->flush();
+    }
+}
---
+++ src/Migrations/Version20191024144214.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191024144214 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE loan (id INT AUTO_INCREMENT NOT NULL, movie_id INT NOT NULL, user_id INT NOT NULL, usable_until DATETIME DEFAULT NULL, number_screenings SMALLINT DEFAULT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_C5D30D038F93B6FC (movie_id), INDEX IDX_C5D30D03A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(200) NOT NULL, genre VARCHAR(60) NOT NULL, viewer_age INT NOT NULL, premiere DATETIME NOT NULL, director VARCHAR(100) NOT NULL, picture VARBINARY(255) DEFAULT NULL, duration SMALLINT NOT NULL, country VARCHAR(50) DEFAULT NULL, description VARCHAR(500) DEFAULT NULL, price_day DOUBLE PRECISION NOT NULL, price_seanse DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(60) NOT NULL, lastname VARCHAR(60) NOT NULL, card_number INT NOT NULL, blocked TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE loan ADD CONSTRAINT FK_C5D30D038F93B6FC FOREIGN KEY (movie_id) REFERENCES movie (id)');
+        $this->addSql('ALTER TABLE loan ADD CONSTRAINT FK_C5D30D03A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE loan DROP FOREIGN KEY FK_C5D30D038F93B6FC');
+        $this->addSql('ALTER TABLE loan DROP FOREIGN KEY FK_C5D30D03A76ED395');
+        $this->addSql('DROP TABLE loan');
+        $this->addSql('DROP TABLE movie');
+        $this->addSql('DROP TABLE user');
+    }
+}
