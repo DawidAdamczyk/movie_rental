58938ae965c0a6b5a3b9313d59c4e80d35f4a211
Dawid <dawidadamczykeit@gmail.com>
2019-10-31 16:47:46 +0100

    add bootsrap

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ Encore
     // require('../css/navbar.css');
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -69,7 +69,13 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
+
+    // .autoProvideVariables({
+    //     $: 'jquery',
+    //     jQuery: 'jquery',
+    //     'window.jQuery': 'jquery',
+    // })
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
